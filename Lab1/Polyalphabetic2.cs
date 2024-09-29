91941053277cb0b26ad8bb2c7ec8bccfa484da39
duy <tranhoangthanhduy@gmail.com>
2024-09-29 14:24:35 +0700

    Add project files.

---
+++ Lab1/Polyalphabetic2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Polyalphabetic2 : Form
+    {
+        public Polyalphabetic2()
+        {
+            InitializeComponent();
+        }
+
+        private void Polyalphabetic2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
