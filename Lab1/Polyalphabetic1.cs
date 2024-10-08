ff650aa5abb228a1f2f02b3627684263b20263cc
RemKouhai <iloveannie2325@gmail.com>
2024-10-09 00:24:15 +0700

    5.1

--- Lab1/Polyalphabetic1.cs
+++ Lab1/Polyalphabetic1.cs
@@ -16,5 +16,93 @@ namespace Lab1
         {
             InitializeComponent();
         }
+
+        private void Polyalphabetic1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ebtn_Click(object sender, EventArgs e)
+        {
+            string plainText = Inputtxt.Text;
+            string key = Keytxt.Text;
+
+            // Mã hóa
+            string cipherText = Encrypt(plainText, key);
+            Outputtxt.Text = cipherText;
+
+        }
+
+        private void Dbtn_Click(object sender, EventArgs e)
+        {
+            string plainText = Inputtxt.Text;
+            string key = Keytxt.Text;
+
+            // Mã hóa
+            string cipherText = Decrypt(plainText, key);
+            Outputtxt.Text = cipherText;
+        }
+
+
+        public static string Encrypt(string plainText, string key)
+        {
+            string encryptedText = "";
+            key = key.ToUpper();
+            int keyLength = key.Length;
+
+            for (int i = 0, j = 0; i < plainText.Length; i++)
+            {
+                char c = plainText[i];
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    encryptedText += (char)(((c + key[j % keyLength] - 2 * offset) % 26) + offset);
+                    j++;
+                }
+            }
+            return encryptedText;
+        }
+
+
+        public static string Decrypt(string cipherText, string key)
+        {
+            string decryptedText = "";
+            key = key.ToUpper();
+            int keyLength = key.Length;
+
+            for (int i = 0, j = 0; i < cipherText.Length; i++)
+            {
+                char c = cipherText[i];
+                if (char.IsLetter(c))
+                {
+                    char offset = char.IsUpper(c) ? 'A' : 'a';
+                    decryptedText += (char)(((c - key[j % keyLength] + 26) % 26) + offset);
+                    j++;
+                }
+            }
+            return decryptedText;
+        }
+
+        private void Keytxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int code = Convert.ToInt32(e.KeyChar);
+
+            if ((code >64 && code < 91) || code==8 )
+            {
+                e.Handled = false;
+            }
+            else e.Handled = true;
+        }
+
+        private void Inputtxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int code = Convert.ToInt32(e.KeyChar);
+
+            if ((code > 64 && code < 91) || code == 8)
+            {
+                e.Handled = false;
+            }
+            else e.Handled = true;
+        }
     }
 }
